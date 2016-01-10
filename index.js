2765126318fbcf66dba196843bdfb49b6a2cd6ed
jmjuanes <josemijuanes@gmail.com>
2016-01-10 17:04:52 +0100

    Add main

---
+++ index.js
@@ -0,0 +1,8 @@
+//Exports to node
+module.exports = {
+  Merge: require('./lib/merge.js'),
+  Convert: require('./lib/convert.js'),
+  Split: require('./lib/split.js'),
+  Read: require('./lib/read.js'),
+  Write: require('./lib/write.js')
+};
