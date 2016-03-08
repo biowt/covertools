85c15608dc5d02597710067c06cc118ee91a0e0e
jmjuanes <josemijuanes@gmail.com>
2016-03-08 11:01:39 +0100

    Remove unused test

--- test/test-convert.js
+++
@@ -1,9 +0,0 @@
-//Import dependencies
-var covConvert = require('../lib/convert.js');
-var covWrite = require('../lib/write.js');
-
-//Convert the mpileup test file
-var c = covConvert('./examples/mpileup.txt');
-
-//Save file
-covWrite('./examples/cmpileup.txt', c);
